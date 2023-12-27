5a7d5bb3293620a6adbccf5ea0ea3f263e96710d
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2023-12-27 14:35:00 +0100

    feat: player movement

---
+++ entities/player/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody3D
+
+const SPEED: float = 5.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Gravity
+	if !is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Movement
+	var input_dir: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
