b240ff96eeb89698f453649299ff207c28e8abfe
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2023-12-29 18:28:27 +0100

    perf: bullet

--- models/bullet.gd
+++ models/bullet.gd
@@ -6,9 +6,10 @@ const SPEED: float = 12
 const TIME_TO_LIVE: float = 5
 
 
-func _physics_process(delta: float):
-	position += get_meta("direction") * SPEED * delta
-
-	# Destroy bullet after TTL
+func _ready():
 	await get_tree().create_timer(TIME_TO_LIVE).timeout
 	queue_free()
+
+
+func _physics_process(delta: float):
+	position += get_meta("direction") * SPEED * delta
